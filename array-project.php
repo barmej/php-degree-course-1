3c4e1af4000bd633d29134ed336fd9b08733faec
Menawer <careless.kw@gmail.com>
2019-09-16 22:10:07 +0300

    C1 L3 V4 R1

---
+++ array-project.php
@@ -0,0 +1,31 @@
+<?php
+
+$students = array();
+
+$students[] = array(
+    'name' => 'زياد',
+    'age' => 12,
+    'country' => 'اليمن'
+);
+
+$students[] = array(
+    'name' => 'لؤي',
+    'age' => 11,
+    'country' => 'الكويت'
+);
+
+$students[] = array(
+    'name' => 'أسماء',
+    'age' => 13,
+    'country' => 'المغرب'
+);
+
+$students[] = array(
+    'name' => 'سلمى',
+    'age' => 12,
+    'country' => 'مصر'
+);
+
+var_dump($students);
+
+?>
