ffefdc562fc0cb934ec0187a3282195fc4e44eab
Menawer <careless.kw@gmail.com>
2019-09-16 21:47:01 +0300

    C1 L3 V1 R2

---
+++ arrays.php
@@ -0,0 +1,18 @@
+<?php
+
+$companies = array('Google', 'Microsoft', 'Apple');
+
+$cars = ['Cadillac', 'Jeep', 'Toyota', 'Auston Martin'];
+
+var_dump($cars);
+
+unset($cars[1]);
+
+var_dump($cars);
+
+$cars[] = 'Kia';
+
+var_dump($cars);
+
+
+?>
