889458e4e4f2b6489cec3f653e3b46e2070f76ed
Menawer <careless.kw@gmail.com>
2019-09-16 22:14:11 +0300

    C1 L4 V1 R2

---
+++ math.php
@@ -0,0 +1,25 @@
+<?php
+
+$two = 2;
+
+$three = 3;
+
+echo $two + $three;
+
+echo "<br>";
+
+echo $two - $three;
+
+echo "<br>";
+
+echo $two * $three;
+
+echo "<br>";
+
+echo $two / $three;
+
+echo "<br>";
+
+echo ($two + $three) * ($two - $three);
+
+?>
